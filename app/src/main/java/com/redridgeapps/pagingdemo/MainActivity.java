1d2d7dc2bdf9610c408b47eed35cc75382317917
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-07-03 10:36:36 +0530

    MainActivity: Setup GitHub service

--- app/src/main/java/com/redridgeapps/pagingdemo/MainActivity.java
+++ app/src/main/java/com/redridgeapps/pagingdemo/MainActivity.java
@@ -3,11 +3,38 @@ package com.redridgeapps.pagingdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.readystatesoftware.chuck.ChuckInterceptor;
+import com.redridgeapps.pagingdemo.api.GitHubService;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    private GitHubService service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupGitHubService();
+    }
+
+    private void setupGitHubService() {
+
+        OkHttpClient client = new OkHttpClient()
+                .newBuilder()
+                .addInterceptor(new ChuckInterceptor(getApplicationContext()))
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com")
+                .client(client)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build();
+
+        service = retrofit.create(GitHubService.class);
     }
 }
